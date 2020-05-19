a1b44c27be7e84e4d31584f7a9ade22de6117402
Kazem <kazem.cheshmi@gmail.com>
2020-05-18 21:22:03 -0400

    Eigen interface for LBL is added

---
+++ eigen_interface/eigen_lbl_driver.cpp
@@ -0,0 +1,33 @@
+//
+// Created by kazem on 2020-05-18.
+//
+
+#include <unsupported/Eigen/SparseExtra>
+#include <Util.h>
+#include "lbl_eigen.h"
+
+
+int main(int argc, char *argv[]){
+
+ std::map<std::string,std::string> qp_args;
+ parse_args(argc, argv, qp_args);
+
+ /// Reading input matrices.
+ Eigen::SparseMatrix<double,Eigen::ColMajor,int> H;
+ Eigen::VectorXd q;
+ std::string message = "Could not load ";
+ if( !Eigen::loadMarket( H, qp_args["H"] ) ){ std::cout<<message<<"H"; return 1; }
+ if( !Eigen::loadMarketVector( q, qp_args["q"] ) ){ std::cout<<message<<"q"; return 1; }
+ // WARNINGL for now, if there is a zero diagonal in H,
+ // the row/col indices of zero diagonal should be there.
+ //TODO: getting more parameters here and replace qs
+
+ /// output vectors
+ Eigen::VectorXd x;
+
+ /// call the wrapper.
+ int ret = eigen::nasoq::linear_solve(H,q,x);
+
+ //print_vec("sol:\n", 0, H.rows(), x.data());
+ return ret;
+}
---
+++ eigen_interface/lbl_eigen.h
@@ -0,0 +1,69 @@
+//
+// Created by kazem on 2020-05-18.
+//
+
+#ifndef NASOQ_LBL_EIGEN_H
+#define NASOQ_LBL_EIGEN_H
+
+#include "linear_solver_wrapper.h"
+
+namespace eigen
+{
+ namespace nasoq
+ {
+  //  Solving Ax=b
+  // Inputs:
+  //   H  n by n sparse Hessian matrix **lower triangle only** (see
+  //     .triangularView<Eigen::Lower>() )
+  //   q  n by 1 vector
+  // Outputs:
+  //   x  n by 1 solution vector
+  // Returns nasoq exit flag
+  //
+  int linear_solve(
+    // Pass inputs by copy so we get non-const and casted data
+    Eigen::SparseMatrix<double,Eigen::ColMajor,int> A,
+    Eigen::Matrix<double,Eigen::Dynamic,1> b,
+    Eigen::Matrix<double,Eigen::Dynamic,1> & x){
+   assert(A.isApprox(A.triangularView<Eigen::Lower>(),0) &&
+          "P should be lower triangular");
+   assert(A.isCompressed());
+   assert(A.rows()==A.cols());
+   assert(A.rows()==b.rows());
+
+   CSC *H = new CSC;
+   H->nzmax = A.nonZeros();
+   H->ncol= H->nrow=A.rows();
+   H->p = A.outerIndexPtr();
+   H->i = A.innerIndexPtr();
+   H->x = A.valuePtr();
+   H->stype=-1;
+   // TODO: fix this
+   H->xtype=CHOLMOD_REAL;
+   H->packed=TRUE;
+   H->nz = NULL;
+   H->sorted = TRUE;
+   int reg_diag = -9;
+
+   SolverSettings *lbl = new SolverSettings(H,b.data());
+   lbl->ldl_variant = 4;
+   lbl->req_ref_iter = 2;
+   lbl->solver_mode = 0;
+   lbl->reg_diag = pow(10,reg_diag);
+   lbl->symbolic_analysis();
+   lbl->numerical_factorization();
+   double *sol = lbl->solve_only();
+   lbl->compute_norms();
+
+   x = Eigen::Map< Eigen::Matrix<double,Eigen::Dynamic,1> >(
+     sol,A.rows(),1);
+
+   //std::cout<<"residual: "<<lbl->res_l1<<"\n";
+   // Exitflag TODO
+   int exitflag = 0;
+   delete lbl;
+   return exitflag;
+  }
+ }
+}
+#endif //NASOQ_LBL_EIGEN_H
